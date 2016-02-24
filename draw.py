2a966ddfd7bcac8e72c80dd481a5413dfb19922d
Justin Pacquing <pacqqu@gmail.com>
2016-02-23 23:43:12 -0500

    wowowee

--- draw.py
+++ draw.py
@@ -10,14 +10,26 @@ def draw_line( screen, x0, y0, x1, y1, color ):
         print slope
         if (slope > 0):
             if (slope < 1):
-                draw_oct1(screen, x0, y0, x1, y1,color)
+                if (x0 < x1):
+                    draw_oct1(screen, x0, y0, x1, y1,color)
+                else:
+                    draw_oct1(screen, x1, y1, x0, y0,color)
             else:
-                draw_oct2(screen, x0, y0, x1, y1,color)
+                if (x0 < x1):
+                    draw_oct2(screen, x0, y0, x1, y1,color)
+                else:
+                    draw_oct2(screen, x1, y1, x0, y0,color)
         else:
             if (slope > -1):
-                draw_oct8(screen, x0, y0, x1, y1,color)
+                if (x0 < x1):
+                    draw_oct8(screen, x0, y0, x1, y1,color)
+                else:
+                    draw_oct8(screen, x1, y1, x0, y0,color)
             else:
-                draw_oct7(screen, x0, y0, x1, y1,color)
+                if (x0 < x1):
+                    draw_oct7(screen, x0, y0, x1, y1,color)
+                else:
+                    draw_oct7(screen, x1, y1, x0, y0,color)
 
 def draw_oct1(screen, x0, y0, x1, y1,color):
     x = x0
@@ -25,7 +37,7 @@ def draw_oct1(screen, x0, y0, x1, y1,color):
     A = y1 - y0
     B = - (x1 - x0)
     d = (2*A) + B
-    while ( x <= x):
+    while ( x <= x1):
         plot(screen, color, x, y)
         if ( d > 0 ):
             y += 1
